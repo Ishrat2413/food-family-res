4b18fee8148408018d580bd29e39d8c8293c2d90
Ishrat2413 <ishratrintu2413@gmail.com>
2025-09-06 13:09:43 +0600

    Project running Successfully

--- food-family-client/src/hooks/useAxiosPublic.jsx
+++ food-family-client/src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://food-family-res.onrender.com/'
 })
 
 const useAxiosPublic = () => {
--- food-family-client/src/pages/Home/Testimonials/Testimonials.jsx
+++ food-family-client/src/pages/Home/Testimonials/Testimonials.jsx
@@ -12,7 +12,7 @@ const Testimonials = () => {
     const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/reviews')
+        fetch('https://food-family-res.onrender.com/reviews')
             .then(res => res.json())
             .then(data => setReviews(data))
     }, []);
